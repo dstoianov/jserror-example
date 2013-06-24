97491196635d2d6a71bb635460d0ad10caddb459
Artem Koshelev <artkoshelev@yandex-team.ru>
2013-06-24 21:04:52 +0400

    initial commit

---
+++ src/test/java/ru/yandex/qatools/jserror/JsErrors.java
@@ -0,0 +1,10 @@
+package ru.yandex.qatools.jserror;
+
+import org.browsermob.proxy.ProxyServer;
+import org.openqa.selenium.WebDriver;
+
+public class JsErrors {
+    private String url;
+    private static ProxyServer proxy;
+    private WebDriver driver;
+}
